ebc293756e97fb8450d1a6a151344666716615a5
cchong0421 <hung.chung.chun@gmail.com>
2023-04-06 21:37:33 +0800

    add middleware

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
 import uvicorn
 import config
+import json
 
 app = FastAPI()
 
+
+class ErrorModel(BaseModel):
+    code: str
+    message: str
+
+
+@app.middleware("http")
+async def check_CustHeader(request, call_next):
+    custHead = request.headers.get("custhead1")
+
+    response = await call_next(request)
+    if not custHead:
+        # response.status_code = status.HTTP_401_UNAUTHORIZED
+        m = ErrorModel(code="401", message="UnAuthorized")
+        print()
+        # return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED,
+        #                     content={"Error": "UnAuthorized"})
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED,
+                            content=json.dumps(m.__dict__),
+                            headers={'Content-type': 'application/json'})
+    return response
+
+
 @app.get("/")
-def root_index():
+async def root_index():
     return {'Hello': f"world --- {config.ServerName}"}
